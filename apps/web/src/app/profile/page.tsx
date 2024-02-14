f44d6340983ee74aac844e36e01df7ca5baf871e
Adrrick <adrikgw2@gmail.com>
2024-02-14 10:14:05 +0100

    feat: :sparkles: Basic protection

--- apps/web/src/app/profile/page.tsx
+++ apps/web/src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import isLogged from '@/actions/isLogged';
 import { useRouter } from 'next/navigation'
 export default function Profile() {
 
@@ -10,6 +11,12 @@ export default function Profile() {
     localStorage.removeItem("token");
     router.push('/login');
 
+  }
+
+  const userState = isLogged();
+
+  if(!userState) {
+    router.push('/login');
   }
 
   return (
