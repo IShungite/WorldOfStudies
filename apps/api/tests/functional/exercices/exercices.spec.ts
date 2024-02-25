a8b74967a2f78d2c84f26302618857c26ad0a060
IShungite <56173955+IShungite@users.noreply.github.com>
2024-02-25 18:24:47 +0100

    refactor: tests structure

--- apps/api/tests/functional/exercices/exercices.spec.ts
+++ apps/api/tests/functional/exercices/exercices.spec.ts
@@ -1,60 +1,34 @@
 import { Exercice } from '#domainModels/exercice/exercice'
-import { questionType } from '#domainModels/exercice/question'
+import { IExercicesRepository } from '#domainPorts/out/exercices.repository'
+import { InMemoryExercicesRepository } from '#repositories/in_memory_exercices.repository'
+import app from '@adonisjs/core/services/app'
 import { test } from '@japa/runner'
-import { Id } from '#domainModels/id'
-import { QuestionFactory } from '#factories/question.factory'
-import { UserAnswerFactory } from '#factories/user_answer.factory'
 
-test.group('Exercices', () => {
-  const questionQCM = QuestionFactory.create({
-    id: new Id('1'),
-    type: questionType.QCM,
-    points: 2,
-    choices: [
-      {
-        id: new Id('1'),
-        label: 'Choice 1',
-        isCorrect: true,
-      },
-      {
-        id: new Id('2'),
-        label: 'Choice 2',
-        isCorrect: false,
-      },
-    ],
-  })
+test.group('Exercices Controller', (group) => {
+  let exercicesRepository: IExercicesRepository
 
-  const questionTextHole = QuestionFactory.create({
-    id: new Id('2'),
-    type: questionType.TEXT_HOLE,
-    text: 'Question 1',
-    points: 2,
-    answers: ['hello', 'world'],
-  })
-
-  const exercice = new Exercice({
-    id: new Id('1'),
-    name: 'My Exo',
-    questions: [questionQCM, questionTextHole],
-  })
-
-  test('It should return the good total amount of points', async ({ assert }) => {
-    const userAnswer1 = UserAnswerFactory.create({
-      type: questionType.QCM,
-      id: new Id('1'),
-      questionId: questionQCM.id,
-      choiceId: questionQCM.choices[0].id,
-      userId: new Id('1'),
+  group.each.setup(async () => {
+    exercicesRepository = new InMemoryExercicesRepository()
+    app.container.swap(IExercicesRepository, () => {
+      return exercicesRepository
     })
+  })
 
-    const userAnswers2 = UserAnswerFactory.create({
-      type: questionType.TEXT_HOLE,
-      id: new Id('1'),
-      questionId: questionTextHole.id,
-      values: ['hello'],
-      userId: new Id('1'),
+  test('It should create an exercice', async ({ client }) => {
+    const response = await client.post('/exercices').json({
+      name: 'Quiz 1',
+      questions: [],
     })
+    response.assertStatus(201)
+    response.assertBodyContains({ name: 'Quiz 1' })
+  })
 
-    assert.equal(exercice.getTotalUserPoints([userAnswer1, userAnswers2]), 3)
+  test('It should return the list of exercices', async ({ client, assert }) => {
+    exercicesRepository.store(new Exercice({ name: 'Quiz 1', questions: [] }))
+    exercicesRepository.store(new Exercice({ name: 'Quiz 2', questions: [] }))
+
+    const response = await client.get('/exercices')
+    response.assertStatus(200)
+    assert.lengthOf(response.body(), 2)
   })
 })
