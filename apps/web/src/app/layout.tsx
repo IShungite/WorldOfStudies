b72df9e1e4135c4ec9471147fe2543434329538a
Adrrick <adrikgw2@gmail.com>
2024-03-01 11:05:34 +0100

    feat: :lipstick: Add nes + font globally

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import "nes.css/css/nes.min.css";
+import localFont from 'next/font/local';
 
-const inter = Inter({ subsets: ["latin"] });
+const PressStart2P = localFont({src: '../asset/PressStart2P/PressStart2P-Regular.ttf'})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={PressStart2P.style}>{children}</body>
     </html>
   );
 }
