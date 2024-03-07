d025f47f72141ff4dc11aeac6a95b02843cdffa0
Adrrick <adrikgw2@gmail.com>
2024-03-07 14:53:07 +0100

    feat: :sparkles: Add delete school by id

--- apps/api/app/domain/ports/out/schools.repository.ts
+++ apps/api/app/domain/ports/out/schools.repository.ts
@@ -5,4 +5,5 @@ export abstract class ISchoolsRepository {
   abstract save(school: School): Promise<School>
   abstract getById(schoolId: Id): Promise<School | null>
   abstract getByPromotionId(promotionId: Id): Promise<School | null>
+  abstract deleteById(schoolId: Id): Promise<void>
 }
