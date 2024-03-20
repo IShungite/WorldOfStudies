72cd2cd14dccdcb0661662b215cf7aa5f1e7d4a1
Adrrick <adrikgw2@gmail.com>
2024-03-20 13:50:35 +0100

    feat: :sparkles: Add update subject service

---
+++ apps/api/app/domain/services/subject/update_subject.service.ts
@@ -0,0 +1,59 @@
+import { Id } from '#domainModels/id/id'
+import { Promotion } from '#domainModels/school/promotion'
+import { PromotionNotFoundException } from '#domainModels/school/promotion_not_found.exception'
+import { School } from '#domainModels/school/school'
+import { SchoolNotFoundException } from '#domainModels/school/school_not_found.exception'
+import { Subject, UpdateSubjectDto } from '#domainModels/school/subject'
+import { SubjectNotFoundException } from '#domainModels/school/subject_not_found.exception'
+import { UpdateSubjectUseCase } from '#domainPorts/in/subject/update_subject.use_case'
+import { ISchoolsRepository } from '#domainPorts/out/schools.repository'
+import { inject } from '@adonisjs/core'
+
+@inject()
+export class UpdateSubjectService implements UpdateSubjectUseCase {
+  constructor(private readonly schoolsRepository: ISchoolsRepository) {}
+
+  async update(
+    schoolId: Id,
+    promotionId: Id,
+    subjectId: Id,
+    updateSubjectDto: UpdateSubjectDto
+  ): Promise<Subject> {
+    const school = await this.schoolsRepository.getById(schoolId)
+
+    if (!school) {
+      throw new SchoolNotFoundException(schoolId)
+    }
+
+    const promotion = school.promotions.find((p) => p.id.equals(promotionId))
+
+    if (!promotion) {
+      throw new PromotionNotFoundException(promotionId)
+    }
+
+    const subject = promotion.subjects.find((s) => s.id.equals(subjectId))
+
+    if (!subject) {
+      throw new SubjectNotFoundException(subjectId)
+    }
+
+    const newSubject = new Subject({
+      id: subject.id,
+      name: updateSubjectDto.name ?? subject.name,
+    })
+
+    const newPromotion = new Promotion({
+      ...promotion,
+      subjects: promotion.subjects.map((s) => (s.id.equals(newSubject.id) ? newSubject : s)),
+    })
+
+    const newSchool = new School({
+      ...school,
+      promotions: school.promotions.map((p) => (p.id.equals(newPromotion.id) ? newPromotion : p)),
+    })
+
+    await this.schoolsRepository.save(newSchool)
+
+    return newSubject
+  }
+}
