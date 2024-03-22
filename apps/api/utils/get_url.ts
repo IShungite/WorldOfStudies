91db56d9466ceb95715303c6ace8e017bd800ee8
Adrrick <adrikgw2@gmail.com>
2024-03-22 15:58:35 +0100

    feat: :sparkles: Add getUrl helper

---
+++ apps/api/utils/get_url.ts
@@ -0,0 +1,4 @@
+import env from '#start/env'
+export function getUrl(route: string) {
+  return `${env.get('NODE_ENV') === 'development' ? 'http' : 'https'}://${env.get('HOST')}:${env.get('PORT')}/${route}`
+}
