b3e6092a1c8de795412858744d6389c080a5fd95
Akram <akram.abdl@outlook.fr>
2024-03-26 12:01:55 +0100

    feat: :wrench: added i18n config file + suspense fallback
    had to add a suspense fallback on the main component so that it has time to load the translations before displaying the page

--- apps/mobile/app/app.tsx
+++ apps/mobile/app/app.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
+import React, { Suspense } from 'react'
+import { ActivityIndicator, SafeAreaView, StyleSheet } from 'react-native'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
-// Import your screens
-import LogInScreen from '../screens/LogInScreen' // Ensure you have this screen created
+import '../i18n/i18n'
+
+import LogInScreen from '../screens/LogInScreen'
 import SignUpScreen from '../screens/SignUpScreen'
 
 const queryClient = new QueryClient()
@@ -13,16 +14,18 @@ const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NavigationContainer>
-        <SafeAreaView style={styles.container}>
-          <Stack.Navigator initialRouteName="SignUp">
-            <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up' }} />
-            <Stack.Screen name="LogIn" component={LogInScreen} options={{ title: 'Log In' }} />
-          </Stack.Navigator>
-        </SafeAreaView>
-      </NavigationContainer>
-    </QueryClientProvider>
+    <Suspense fallback={<ActivityIndicator size="large" />}>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <SafeAreaView style={styles.container}>
+            <Stack.Navigator initialRouteName="SignUp">
+              <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up' }} />
+              <Stack.Screen name="LogIn" component={LogInScreen} options={{ title: 'Log In' }} />
+            </Stack.Navigator>
+          </SafeAreaView>
+        </NavigationContainer>
+      </QueryClientProvider>
+    </Suspense>
   )
 }
 
