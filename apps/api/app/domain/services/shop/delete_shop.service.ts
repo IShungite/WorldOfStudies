f671d220a324ea07718f876943d65875a1f4362a
Adrrick <adrikgw2@gmail.com>
2024-03-28 15:01:44 +0100

    feat: :sparkles: Added delete shop service

---
+++ apps/api/app/domain/services/shop/delete_shop.service.ts
@@ -0,0 +1,20 @@
+import { Id } from '#domainModels/id/id'
+import { ShopNotFoundException } from '#domainModels/shop/shop_not_found_exception'
+import { DeleteShopUseCase } from '#domainPorts/in/shop/delete_shop.use_case'
+import { IShopsRepository } from '#domainPorts/out/shops.repository'
+import { inject } from '@adonisjs/core'
+
+@inject()
+export class DeleteShopService implements DeleteShopUseCase {
+  constructor(private readonly shopRepository: IShopsRepository) {}
+
+  async delete(schoolId: Id): Promise<void> {
+    const shop = await this.shopRepository.getBySchoolId(schoolId)
+
+    if (!shop) {
+      throw new ShopNotFoundException()
+    }
+
+    await this.shopRepository.deleteById(shop.id)
+  }
+}
