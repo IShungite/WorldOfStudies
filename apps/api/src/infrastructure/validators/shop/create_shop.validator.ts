b2aeeb0103a80ec6982b18de88fc319414364fe5
Adrrick <adrikgw2@gmail.com>
2024-03-29 16:55:03 +0100

    refactor: :recycle: Updated create shop validator

--- apps/api/src/infrastructure/validators/shop/create_shop.validator.ts
+++ apps/api/src/infrastructure/validators/shop/create_shop.validator.ts
@@ -2,12 +2,12 @@ import vine from '@vinejs/vine'
 import { domainIdValidator } from '#infrastructure/validators/domain_id.validator'
 import { Price } from '#domain/models/shop/price'
 
-const createShopProductValidator = vine.object({
+export const createShopProductValidator = vine.object({
   name: vine.string(),
   price: vine.number().transform((value) => new Price(value)),
 })
 
-const createShopCategoryValidator = vine.object({
+export const createShopCategoryValidator = vine.object({
   name: vine.string(),
   products: vine.array(createShopProductValidator),
 })
