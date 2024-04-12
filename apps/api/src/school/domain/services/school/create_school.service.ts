936d38e90c69ca1e59c0745319a908d7a92a48a4
Adrrick <adrikgw2@gmail.com>
2024-04-12 11:59:16 +0200

    feat: :sparkles: Updated school entity,model and save

--- apps/api/src/school/domain/services/school/create_school.service.ts
+++ apps/api/src/school/domain/services/school/create_school.service.ts
@@ -1,13 +1,36 @@
 import { inject } from '@adonisjs/core'
 import { ISchoolsRepository } from '#school/domain/contracts/repositories/schools.repository'
 import { CreateSchoolDto, School } from '#school/domain/models/school'
+import { User } from '#user/domain/models/user'
+import { role } from '#user/domain/models/role'
+import { IUsersRepository } from '#user/domain/contracts/repositories/users.repository'
 
 @inject()
 export class CreateSchoolService {
-  constructor(private readonly schoolsRepository: ISchoolsRepository) {}
+  constructor(
+    private readonly schoolsRepository: ISchoolsRepository,
+    private readonly usersRepository: IUsersRepository
+  ) {}
 
   async execute(createSchoolDto: CreateSchoolDto): Promise<School> {
-    const school = new School(createSchoolDto)
+    const templateAdmin = new User({
+      firstName: 'admin',
+      lastName: 'admin',
+      role: role.ADMIN,
+      email: '',
+      password: 'password',
+    })
+
+    const admin = new User({
+      ...templateAdmin,
+      email: `${templateAdmin.id}@worldofstudies.com`,
+    })
+
+    const dtoWithAdmin = { ...createSchoolDto, admins: [admin] }
+
+    await this.usersRepository.save(admin)
+
+    const school = new School(dtoWithAdmin)
 
     return this.schoolsRepository.save(school)
   }
--- apps/api/src/school/infrastructure/entities/school.ts
+++ apps/api/src/school/infrastructure/entities/school.ts
@@ -1,8 +1,9 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
-import type { HasMany } from '@adonisjs/lucid/types/relations'
+import { BaseModel, column, hasMany, manyToMany } from '@adonisjs/lucid/orm'
+import type { HasMany, ManyToMany } from '@adonisjs/lucid/types/relations'
 import PromotionEntity from '#school/infrastructure/entities/promotion'
 import ShopEntity from '#shop/infrastructure/entities/shop'
+import User from '#user/infrastructure/entities/user'
 
 export default class School extends BaseModel {
   @column({ isPrimary: true })
@@ -16,6 +17,9 @@ export default class School extends BaseModel {
 
   @hasMany(() => ShopEntity)
   declare shop: HasMany<typeof ShopEntity>
+
+  @manyToMany(() => User, { pivotTable: 'school_admin' })
+  declare admins: ManyToMany<typeof User>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
