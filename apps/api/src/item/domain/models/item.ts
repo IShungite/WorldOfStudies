943d1a0f4e8b62d83e0b87dd7f1fe9bd22801419
Adrrick <adrikgw2@gmail.com>
2024-09-01 21:38:58 +0200

    feat: :card_file_box: Add type column to item entity

--- apps/api/src/item/domain/models/item.ts
+++ apps/api/src/item/domain/models/item.ts
@@ -1,16 +1,20 @@
+import { ItemType } from '#shared/enums/item_type'
 import { Id } from '#shared/id/domain/models/id'
 
 type ItemProps = {
   id?: Id
   name: string
+  type: ItemType
 }
 
 export class Item {
   readonly id: Id
   readonly name: string
+  readonly type: ItemType
 
-  constructor({ id, name }: ItemProps) {
+  constructor({ id, name, type }: ItemProps) {
     this.id = id ?? Id.factory()
     this.name = name
+    this.type = type
   }
 }
--- apps/api/src/item/infrastructure/entities/item.ts
+++ apps/api/src/item/infrastructure/entities/item.ts
@@ -1,3 +1,4 @@
+import { ItemType } from '#shared/enums/item_type'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { DateTime } from 'luxon'
 
@@ -7,6 +8,9 @@ export default class Item extends BaseModel {
 
   @column()
   declare name: string
+
+  @column()
+  declare type: ItemType
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
