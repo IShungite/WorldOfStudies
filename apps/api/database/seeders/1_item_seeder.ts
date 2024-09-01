247593659d196e6ffb42561b8eddeb65850b9d17
Adrrick <adrikgw2@gmail.com>
2024-09-01 21:39:36 +0200

    chore: :seedling: Update item seeder

--- apps/api/database/seeders/1_item_seeder.ts
+++ apps/api/database/seeders/1_item_seeder.ts
@@ -3,12 +3,14 @@ import { Id } from '#shared/id/domain/models/id'
 import app from '@adonisjs/core/services/app'
 import { IItemRepository } from '#item/domain/contracts/items_repository.contract'
 import { Item } from '#item/domain/models/item'
+import { ItemType } from '#shared/enums/item_type'
 
 export const items = Array.from({ length: 7 }, (_, i) => {
   const num = `${i + 1}`
   return new Item({
     id: new Id(num),
     name: `Item ${num}`,
+    type: ItemType.Misc,
   })
 })
 
