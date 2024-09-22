5d8fa356c514b52fec221ab0d4a36f99ba7f4c57
Adrrick <adrikgw2@gmail.com>
2024-09-22 17:48:32 +0200

    chore: updated item seeder to feature skins and misc

--- apps/api/database/seeders/1_item_seeder.ts
+++ apps/api/database/seeders/1_item_seeder.ts
@@ -2,24 +2,64 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 import { Id } from '#shared/id/domain/models/id'
 import app from '@adonisjs/core/services/app'
 import { IItemRepository } from '#item/domain/contracts/items_repository.contract'
-import { Item } from '#item/domain/models/item'
 import { ItemType } from '#shared/enums/item_type'
+import { Item } from '#item/domain/models/item'
 
-export const items = Array.from({ length: 7 }, (_, i) => {
-  const num = `${i + 1}`
-  return new Item({
-    id: new Id(num),
-    name: `Item ${num}`,
-    type: ItemType.Misc,
+export const items: Item[] = [
+  new Item({
+    id: new Id('1'),
+    name: 'Item 1',
+    type: ItemType.Skin,
     image: 'skin1.png',
-    icon: 'https://cdn-icons-png.flaticon.com/512/286/286627.png',
-  })
-})
+    icon: 'skin-icon.png',
+  }),
+  new Item({
+    id: new Id('2'),
+    name: 'Item 2',
+    type: ItemType.Skin,
+    image: 'skin2.png',
+    icon: 'skin-icon.png',
+  }),
+  new Item({
+    id: new Id('3'),
+    name: 'Item 3',
+    type: ItemType.Skin,
+    image: 'skin3.png',
+    icon: 'skin-icon.png',
+  }),
+  new Item({
+    id: new Id('4'),
+    name: 'Item 4',
+    type: ItemType.Skin,
+    image: 'skin4.png',
+    icon: 'skin-icon.png',
+  }),
+  new Item({
+    id: new Id('5'),
+    name: 'Item 5',
+    type: ItemType.Misc,
+    image: 'les-des.png',
+    icon: 'les-des.png',
+  }),
+  new Item({
+    id: new Id('6'),
+    name: 'Item 6',
+    type: ItemType.Misc,
+    image: 'les-des.png',
+    icon: 'les-des.png',
+  }),
+  new Item({
+    id: new Id('7'),
+    name: 'Item 7',
+    type: ItemType.Misc,
+    image: 'les-des.png',
+    icon: 'les-des.png',
+  }),
+]
 
 export default class extends BaseSeeder {
   async run() {
     const repo = await app.container.make(IItemRepository)
-
     await Promise.all(items.map((item) => repo.save(item)))
   }
 }
