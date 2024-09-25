855f9d943b4d33399943875d646b3607bbf0e0a6
Adrrick <adrikgw2@gmail.com>
2024-09-25 12:04:32 +0200

    refactor: export Characters to reuse in other seeders

--- apps/api/database/seeders/3_character_seeder.ts
+++ apps/api/database/seeders/3_character_seeder.ts
@@ -4,36 +4,36 @@ import app from '@adonisjs/core/services/app'
 import { Character } from '#character/domain/models/character'
 import { ICharactersRepository } from '#character/domain/contracts/repositories/characters.repository'
 
+export const characters: Character[] = [
+  new Character({
+    id: new Id('1'),
+    name: 'Character 1',
+    promotionId: new Id('1'),
+    userId: new Id('2'),
+    berries: 100,
+    skin: 'default-skin.png',
+  }),
+  new Character({
+    id: new Id('2'),
+    name: 'Character 2',
+    promotionId: new Id('2'),
+    userId: new Id('2'),
+    berries: 100,
+    skin: 'default-skin.png',
+  }),
+  new Character({
+    id: new Id('3'),
+    name: 'Character 3',
+    promotionId: new Id('1'),
+    userId: new Id('3'),
+    berries: 100,
+    skin: 'default-skin.png',
+  }),
+]
+
 export default class extends BaseSeeder {
   async run() {
     const repo = await app.container.make(ICharactersRepository)
-
-    const characters: Character[] = [
-      new Character({
-        id: new Id('1'),
-        name: 'Character 1',
-        promotionId: new Id('1'),
-        userId: new Id('2'),
-        berries: 100,
-        skin: 'default-skin.png',
-      }),
-      new Character({
-        id: new Id('2'),
-        name: 'Character 2',
-        promotionId: new Id('2'),
-        userId: new Id('2'),
-        berries: 100,
-        skin: 'default-skin.png',
-      }),
-      new Character({
-        id: new Id('3'),
-        name: 'Character 3',
-        promotionId: new Id('1'),
-        userId: new Id('3'),
-        berries: 100,
-        skin: 'default-skin.png',
-      }),
-    ]
     await Promise.all(characters.map((character) => repo.save(character)))
   }
 }
